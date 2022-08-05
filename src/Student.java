aa70a98a29ef7a2586b902885705841d55661940
JH9043 <whdgkr9070@naver.com>
2022-08-05 14:36:57 +0900

    성적관리프로그램 프로젝트 생성

---
+++ src/Student.java
@@ -0,0 +1,102 @@
+
+public class Student {
+
+	private String hakbun;
+	private String name;
+	private int kor, eng, mat, edp, tot;
+	private double avg;
+	private char grade;
+
+	public Student() {
+
+	}
+
+	public String getHakbun() {
+		return hakbun;
+	}
+
+	public void setHakbun(String hakbun) {
+		this.hakbun = hakbun;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMat() {
+		return mat;
+	}
+
+	public void setMat(int mat) {
+		this.mat = mat;
+	}
+
+	public int getEdp() {
+		return edp;
+	}
+
+	public void setEdp(int edp) {
+		this.edp = edp;
+	}
+
+	public int getTot() {
+		return tot;
+	}
+
+	public void setTot(int tot) {
+		this.tot = tot;
+	}
+
+	public double getAvg() {
+		return avg;
+	}
+
+	public void setAvg(double avg) {
+		this.avg = avg;
+	}
+
+	public char getGrade() {
+		return grade;
+	}
+
+	public void setGrade(char grade) {
+		this.grade = grade;
+	}
+
+	@Override
+	public String toString() { //%10s =오른정렬로 10자리 받겠다
+							   //%-10s =왼쪽정렬로 10자리 받겠다
+		return String.format("%-10s\t%10s\t%5d\t%5d\t%5d\t%5d\t\t%5d\t%8.2f\t%3c%n",
+				hakbun, name, kor, eng, mat, edp, tot, avg, grade);
+	}
+
+	public Student(String hakbun, String name, int kor, int eng, int mat, int edp) {
+		this.hakbun = hakbun;
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.mat = mat;
+		this.edp = edp;
+	}
+
+}
